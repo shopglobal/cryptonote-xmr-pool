a431a145f2b43844e8b50d53940b9e6530d13c68
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-17 21:39:41 -0500

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -3,3 +3,7 @@ cd cnpool
 git pull origin master
 git pull origin mynt-patch
 git checkout mynt-patch
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+nvm install 0.10.48
+nvm use 0.10.48 && npm install
+echo 'DONE, ready for action'
