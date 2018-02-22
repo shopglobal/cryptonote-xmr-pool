145fc5ebd2dcdd249ceff327b8f03676ebf8b7d3
Mark Allen Evans <president@worldvaporexpo.com>
2018-02-22 16:45:35 -0500

    app ui prepared for pre-launch

---
+++ appui/config.js
@@ -0,0 +1,17 @@
+var api = "http://165.227.189.226:8117";
+var coinUnits = 1000000000000;
+var poolHost = "Coinmine.network";
+
+var irc = "irc.freenode.net/#mynt-pools";
+
+var email = "support@Coinmine.network";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default-theme.css";
