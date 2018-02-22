dfc6ae37e148177119b03f63afb749a860f0c6f1
Mark Allen Evans <president@worldvaporexpo.com>
2018-02-22 21:33:28 +0000

    Post v2 transactions are 98% successful

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "http://127.0.0.1:1117";
+var coinUnits = 1000000000000;
+var poolHost = "Coinmine.network";
+
+var irc = "irc.freenode.net/#mynt-pools";
+
+var email = "support@Coinmine.network";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default-theme.css";
