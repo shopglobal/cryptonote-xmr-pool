bdfec1b063c284a530f501b4b4499a273074e09e
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-12 18:44:55 -0400

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-git clone https://github.com/shopglobal/cryptonote-universal-pool cnpool 
+git clone https://github.com/mynt-project/cryptonote-universal-pool-v2 cnpool 
 cd cnpool 
 git pull origin master
-git pull origin mynt-patch
-git checkout mynt-patch
 sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 echo 'Adding NVM to the Path, (tested on ubuntu 16.04, otherwise just restart)'
 export NVM_DIR="$HOME/.nvm"
@@ -11,6 +9,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "NVM_DIR/bash_completion"
 echo 'DONE, NVM is ready for action'
 nvm install 0.10.48
-nvm use 0.10.48 && npm install
+nvm use 0.10.48 && npm update && npm install
 echo 'DONE, now start up the pool.'
 
